54d0e446adf32c8f17f5f95985f9148a46e63788
yuhao <lidaopang1412@gmail.com>
2024-08-05 18:42:27 +0800

    代码优化

--- src/main/java/com/jzo2o/api/market/CouponApi.java
+++ src/main/java/com/jzo2o/api/market/CouponApi.java
@@ -1,8 +1,13 @@
 package com.jzo2o.api.market;
 
+import com.jzo2o.api.market.dto.request.CouponUseBackReqDTO;
+import com.jzo2o.api.market.dto.request.CouponUseReqDTO;
 import com.jzo2o.api.market.dto.response.AvailableCouponsResDTO;
+import com.jzo2o.api.market.dto.response.CouponUseResDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
@@ -18,4 +23,18 @@ public interface CouponApi {
      */
     @GetMapping("/getAvailable")
     List<AvailableCouponsResDTO> getAvailable(@RequestParam("totalAmount") BigDecimal totalAmount);
+
+
+    /**
+     * 优惠券使用,并返回优惠金额
+     * @param couponUseReqDTO
+     */
+    @PostMapping("/use")
+    CouponUseResDTO use(@RequestBody CouponUseReqDTO couponUseReqDTO);
+
+    /**
+     * 优惠券退款回退
+     */
+    @PostMapping("/useBack")
+    void useBack(@RequestBody CouponUseBackReqDTO couponUseBackReqDTO);
 }
